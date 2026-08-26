8373166d285691ba242c371926c3a52fdfc4b788
Maksym <svitberezuk2007@gmail.com>
2026-08-26 19:39:12 +0300

    add counting accuracy and errors. Add modal window

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 let keys = document.querySelectorAll(".key");
+let mistakes = 0;
+let correct = 0;
+let mistakesElement = document.getElementById("error-count");
+let accuracyElement = document.getElementById("accuracy");
 document.addEventListener("keydown",(e)=>{
-    if (e.key === characters[currentIndex]){
+    if(currentIndex >= characters.length-1){
+        spans[currentIndex].classList.remove("current")
+        if (e.key === characters[currentIndex]){
+            spans[currentIndex].classList.add("correct")
+        }else{
+        }
+        let choice = confirm("Retry?")
+        if(choice === true){
+            window.location.reload();
+        }
+    }else{
+        if (e.key === characters[currentIndex]){
         spans[currentIndex].classList.remove("current")
         spans[currentIndex].classList.add("correct")
+        correct++
         currentIndex++
         spans[currentIndex].classList.add("current")
 }else{
 spans[currentIndex].classList.add("wrong")
+mistakes++
+mistakesElement.textContent = mistakes
 }
+let accuracy = ((correct/(correct+mistakes))*100);
+        accuracyElement.textContent = Math.floor(accuracy)
+    }
+
 let keyElement = document.querySelector(`[data-key="${e.key}"]`)
 keyElement.classList.add("active")
 })
@@ -17,9 +39,9 @@ document.addEventListener("keyup",(e)=>{
 })
 
 let text = document.querySelector(".text");
-const textVariable = 'the quick brown fox jumps over the lazy dog.';
+const textVariable ="Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
 let currentIndex = 0;
-const characters = textVariable.split("");
+const characters = textVariable.toLowerCase().split("");
 for (const character of characters){
     const span = document.createElement("span");
     span.textContent = character;
@@ -27,4 +49,3 @@ for (const character of characters){
 }
 let spans = document.querySelectorAll(".text span")
 spans[currentIndex].classList.add("current")
-
