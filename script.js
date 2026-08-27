96f52a944a3c601bab2d003ce537b2495afbc3cc
Maksym <svitberezuk2007@gmail.com>
2026-08-27 16:53:04 +0300

    changed UI and added smoother animation of typing

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let mistakes = 0;
 let correct = 0;
 let mistakesElement = document.getElementById("error-count");
 let accuracyElement = document.getElementById("accuracy");
+let WPM = document.getElementById("WPM");
 document.addEventListener("keydown",(e)=>{
     if(currentIndex >= characters.length-1){
         spans[currentIndex].classList.remove("current")
@@ -16,20 +17,24 @@ document.addEventListener("keydown",(e)=>{
         }
     }else{
         if (e.key === characters[currentIndex]){
+            spans[currentIndex].classList.remove("wrong")
         spans[currentIndex].classList.remove("current")
         spans[currentIndex].classList.add("correct")
         correct++
         currentIndex++
         spans[currentIndex].classList.add("current")
+
 }else{
 spans[currentIndex].classList.add("wrong")
 mistakes++
 mistakesElement.textContent = mistakes
 }
-let accuracy = ((correct/(correct+mistakes))*100);
-        accuracyElement.textContent = Math.floor(accuracy)
-    }
 
+    let accuracy = ((correct/(correct+mistakes))*100);
+    accuracyElement.textContent = Math.floor(accuracy)
+
+    WPM.textContent =Math.floor((correct/5)/(30/60));
+}
 let keyElement = document.querySelector(`[data-key="${e.key}"]`)
 keyElement.classList.add("active")
 })
@@ -39,7 +44,7 @@ document.addEventListener("keyup",(e)=>{
 })
 
 let text = document.querySelector(".text");
-const textVariable ="Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+const textVariable ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets.";
 let currentIndex = 0;
 const characters = textVariable.toLowerCase().split("");
 for (const character of characters){
@@ -49,3 +54,19 @@ for (const character of characters){
 }
 let spans = document.querySelectorAll(".text span")
 spans[currentIndex].classList.add("current")
+
+
+const light = document.getElementById("light")
+const dark = document.getElementById("dark")
+dark.classList.add("active")
+light.addEventListener('click',()=>{
+    document.body.style.background = "whitesmoke"
+    dark.classList.remove("active")
+    light.classList.add("active")
+})
+dark.addEventListener('click',()=>{
+    light.classList.remove("active")
+    dark.classList.add("active")
+    document.body.style.background = "#121212"
+
+})
